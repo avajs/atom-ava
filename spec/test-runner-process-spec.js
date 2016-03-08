b872b854be4c7e3b8ad88ce48aa518dc4cd9acb0
Jacob84 <jacob.mendoza@gmail.com>
2016-03-08 22:32:57 +0000

    Conversion of test-runner-process.js to ES6

---
+++ spec/test-runner-process-spec.js
@@ -0,0 +1,46 @@
+'use babel';
+
+const TestRunnerProcess = require('../lib/test-runner-process');
+const TerminalCommandExecutor = require('../lib/terminal-command-executor');
+
+describe('TestRunnerProcess', () => {
+  let runner = {};
+  let executor = {};
+  let parser = {};
+
+  beforeEach(() => {
+    executor = new TerminalCommandExecutor();
+
+    parser = {
+      on(eventName, callBack) {},
+      write() {},
+      end() {}
+    };
+
+    runner = new TestRunnerProcess(executor);
+    spyOn(runner, 'getParser').andReturn(parser);
+  });
+
+  it('can be created', () => expect(runner).not.toBeNull());
+
+  it('runs the executor with the appropriate parameters', () => {
+    spyOn(atom.project, 'getPaths').andReturn(['path']);
+    spyOn(executor, 'run');
+    runner.run('/somefolder/filename');
+    expect(executor.run).toHaveBeenCalledWith('ava filename --tap', '/somefolder/');
+  });
+
+  it('redirects the output for the parser when is received', () => {
+    spyOn(parser, 'write');
+    runner.run('/somefolder/filename');
+    executor.stdOutDataReceived('newdata');
+    expect(parser.write).toHaveBeenCalledWith('newdata');
+  });
+
+  it('closes the parser stream when the output is over', () => {
+    spyOn(parser, 'end');
+    runner.run('/somefolder/filename');
+    executor.streamClosed(0);
+    expect(parser.end).toHaveBeenCalled();
+  });
+});
